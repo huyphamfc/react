4bb5505ee2750b777d7d481d255490618722bc7a
huyphamfc <huyphamfc@gmail.com>
2022-07-13 08:42:07 +0700

    Expenses in Years - Expense Date Component

--- section--01--expenses-in-years/src/components/ExpenseItem.js
+++ section--01--expenses-in-years/src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem({ expenses }) {
-    return expenses.map(({ date, title, amount }) => (
+function ExpenseItem({ date, title, amount }) {
+    return (
         <div className="expense-item">
-            <div>{date.toISOString()}</div>
+            <ExpenseDate date={date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${amount}</div>
             </div>
         </div>
-    ));
+    );
 }
 
 export default ExpenseItem;
