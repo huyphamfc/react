649dc4fc646a9bac66322262fbba4c2c943810b1
huyphamfc <huyphamfc@gmail.com>
2022-07-14 11:11:51 +0700

    Expenses in Years - Click Event

--- section--01--expenses-in-years/src/components/Expenses/ExpenseItem.js
+++ section--01--expenses-in-years/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem({ date, title, amount }) {
+    const [titleState, setTitle] = useState(title);
+
+    const clickHandler = () => {
+        setTitle('Title was changed!');
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{titleState}</h2>
                 <div className="expense-item__price">${amount}</div>
             </div>
+            <button onClick={clickHandler}>
+                Change Title
+            </button>
         </Card>
     );
 }
