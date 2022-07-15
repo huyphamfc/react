d716951e03341de5dc76ded700060e5ed8bb799f
huyphamfc <huyphamfc@gmail.com>
2022-07-15 07:50:54 +0700

    Two-way Binding - Random Gift

--- 007--two-way-binding/src/App.js
+++ 007--two-way-binding/src/App.js
@@ -1,5 +1,24 @@
-function App() {
+import { useState } from 'react';
 
+const gifts = [
+  'CPU i9',
+  'RAM 32GB',
+  'Keyboard RGB'
+];
+
+function App() {
+  const [gift, setGift] = useState();
+
+  const clickHandler = () => {
+    setGift(gifts[Math.trunc(Math.random() * gifts.length)]);
+  }
+
+  return (
+    <div>
+      <h1>{gift || 'Do not have any gift'}</h1>
+      <button onClick={clickHandler}>Click Me</button>
+    </div>
+  )
 }
 
 export default App;
