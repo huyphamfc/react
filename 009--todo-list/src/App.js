b1ee7ba026e8568c629c024c83ffc93714d9aa47
huyphamfc <huyphamfc@gmail.com>
2022-07-19 14:07:42 +0700

    Todo List - implement Add button

--- 009--todo-list/src/App.js
+++ 009--todo-list/src/App.js
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+
 function App() {
-  return (
-    <h1>Hello World</h1>
+  const [inputValue, setInputValue] = useState('');
+  const [todo, setTodo] = useState(() =>
+    JSON.parse(localStorage.getItem('todo')) ?? []
   );
+  const clickHandler = () => {
+    if (!inputValue) return;
+    const todoUpdate = [...todo, inputValue];
+    const todoStorage = JSON.stringify(todoUpdate);
+    localStorage.setItem('todo', todoStorage);
+    setTodo(todoUpdate);
+    setInputValue('');
+  }
+
+  return (
+    <div>
+      <input
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
+      <button onClick={clickHandler}>Add</button>
+      <ul>
+        {
+          todo.map(
+            (item, index) =>
+              <li key={index}>{item}</li>
+          )
+        }
+      </ul>
+    </div>
+  )
 }
 
 export default App;
