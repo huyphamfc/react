0b5ddfc8bf16b4a46cf14d169ec8eeeffa7ef02e
huyphamfc <huyphamfc@gmail.com>
2022-07-20 10:25:22 +0700

    useEffect - useEffect(callback,[])

--- 011--use-effect/src/Component.js
+++ 011--use-effect/src/Component.js
@@ -3,6 +3,8 @@ Case 1: the callback is called
             + after the component was mounted
               (the component added elements to DOM)
             + whenever the component was re-render
+
+Case 2: the callback is called only once after the component was mounted
 */
 
 import {
@@ -12,13 +14,16 @@ import {
 
 function Component() {
     const [title, setTitle] = useState('');
+    const [posts, setPosts] = useState([]);
 
     useEffect(
         () => {
-            console.log('Re-render');
-            document.title = title;
-        }
-    )
+            fetch(`https://jsonplaceholder.typicode.com/posts`)
+                .then(res => res.json())
+                .then(data => setPosts(data));
+        },
+        []
+    );
 
     return (
         <div>
@@ -27,7 +32,15 @@ function Component() {
                 value={title}
                 onChange={e => setTitle(e.target.value)}
             />
-            {console.log('Render')}
+            <ul>
+                {
+                    posts.map(post =>
+                        <li key={post.id}>
+                            {post.title}
+                        </li>
+                    )
+                }
+            </ul>
         </div>
     );
 }
