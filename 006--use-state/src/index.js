4f503f787903be56f0fa4e2e385bc5a9fa38550d
huyphamfc <huyphamfc@gmail.com>
2022-07-21 10:19:23 +0700

    useState - behind the scenes

--- 006--use-state/src/index.js
+++ 006--use-state/src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+
+let state = [];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function renderUI() {
+  root.render(<App />);
+}
+
+function useState(initialValue) {
+  function setValue(newValue) {
+    state[0] = newValue;
+    renderUI();
+  }
+  if (state.length) return state;
+  return state = [initialValue, setValue];
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        +
+      </button>
+    </div>
+  )
+}
+
+renderUI();
