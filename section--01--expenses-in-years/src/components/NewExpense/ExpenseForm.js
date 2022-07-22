811adaf6db0f8a8ba779810dfe9a592fa52a4437
huyphamfc <huyphamfc@gmail.com>
2022-07-22 16:59:41 +0700

    Expenses in Years - implement form submission

--- section--01--expenses-in-years/src/components/NewExpense/ExpenseForm.js
+++ section--01--expenses-in-years/src/components/NewExpense/ExpenseForm.js
@@ -1,27 +1,54 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+    const [titleInput, setTitleInput] = useState('');
+    const [amountInput, setAmountInput] = useState('');
+    const [dateInput, setDateInput] = useState('');
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        const expenseData = {
+            title: titleInput,
+            amount: amountInput,
+            date: new Date(dateInput)
+        }
+
+        setTitleInput('');
+        setAmountInput('');
+        setDateInput('');
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input
+                        value={titleInput}
+                        type="text"
+                        onChange={e => setTitleInput(e.target.value)}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
                     <input
+                        value={amountInput}
                         type="number"
                         min="0.01"
                         step="0.01"
+                        onChange={e => setAmountInput(e.target.value)}
                     />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
                     <input
+                        value={dateInput}
                         type="date"
                         min="2020-01-01"
-                        max="2020-12-31"
+                        max="2022-12-31"
+                        onChange={e => setDateInput(e.target.value)}
                     />
                 </div>
             </div>
