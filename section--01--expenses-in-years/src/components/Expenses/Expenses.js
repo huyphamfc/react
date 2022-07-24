5e544fc92bd1ecca4b8e7ae298371595a85aeb9e
huyphamfc <huyphamfc@gmail.com>
2022-07-24 14:29:57 +0700

    Expenses in Years - ExpenseFilter Component

--- section--01--expenses-in-years/src/components/Expenses/Expenses.js
+++ section--01--expenses-in-years/src/components/Expenses/Expenses.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 function Expenses({ expensesRecord }) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+
     const expenses = expensesRecord.map((obj, index) =>
         <ExpenseItem
             key={index}
@@ -11,6 +19,10 @@ function Expenses({ expensesRecord }) {
 
     return (
         <Card className='expenses'>
+            <ExpenseFilter
+                selected={filteredYear}
+                onChangeFilter={filterChangeHandler}
+            />
             {expenses}
         </Card>
     );
