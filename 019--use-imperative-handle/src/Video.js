51ced3a8aa0ea2a0c0250aae474039026ea31cce
huyphamfc <huyphamfc@gmail.com>
2022-08-03 10:27:38 +0700

    useImperativeHandle - concept

--- 019--use-imperative-handle/src/Video.js
+++ 019--use-imperative-handle/src/Video.js
@@ -1,11 +1,22 @@
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import video from './videos/tiktok.mp4';
 
 
 function Video(props, ref) {
+    const videoRef = useRef();
+
+    useImperativeHandle(ref, () => ({
+        play() {
+            videoRef.current.play();
+        },
+        pause() {
+            videoRef.current.pause();
+        }
+    }));
+
     return (
         <video
-            ref={ref}
+            ref={videoRef}
             src={video}
             width={240}
         />
