fd106aefa8ca2f71808fa60ee1c8b1ea1157213c
huyphamfc <huyphamfc@gmail.com>
2022-08-03 09:01:56 +0700

    Global State - Todo List v3

--- 018--global-state/src/App.js
+++ 018--global-state/src/App.js
@@ -1,9 +1,29 @@
-import { useStore } from "./store/hooks";
+import { useStore, actions } from "./store";
 
 
 export default function App() {
     const [state, dispatch] = useStore();
+
+    const addHandler = () => {
+        dispatch(actions.addTodo(state.todoInput));
+    }
+
     return (
-        <h1>Hello, React!</h1>
+        <div>
+            <h1>Todo List</h1>
+            <input
+                value={state.todoInput}
+                placeholder='Enter todo ...'
+                onChange={e => {
+                    dispatch(actions.setTodoInput(e.target.value));
+                }}
+            />
+            <button onClick={addHandler}>Add</button>
+            <ul>
+                {
+                    state.todo.map((todo, index) => <li key={index}>{todo}</li>)
+                }
+            </ul>
+        </div>
     );
 }
