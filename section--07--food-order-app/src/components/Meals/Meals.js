84ef37df0e3f779fe696c855494891a6556a1e36
huyphamfc <huyphamfc@gmail.com>
2022-08-17 16:01:26 +0700

    Food Order - build Meals component

---
+++ section--07--food-order-app/src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+
+function Meals() {
+    return (<>
+        <MealsSummary />
+        <AvailableMeals />
+    </>);
+}
+
+export default Meals;
