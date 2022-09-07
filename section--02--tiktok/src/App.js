2536f132046ce849216477a0905a7b5a898af6e4
huyphamfc <huyphamfc@gmail.com>
2022-09-07 17:03:20 +0700

    TikTok - config SCSS

---
+++ section--02--tiktok/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+    return (<h1>Hello, TikTok!</h1>);
+}
+
+export default App;
--- section--02--tiktok/src/index.js
+++ section--02--tiktok/src/index.js
@@ -1,8 +1,15 @@
 import ReactDOM from 'react-dom/client';
 
+import GlobalStyles from './components/GlobalStyles';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('root')
 );
 
-root.render(<h1>Hello, TikTok!</h1>);
+root.render(
+    <GlobalStyles>
+        <App />
+    </GlobalStyles>
+);
