f8b57b614fd1b8d1294482b8c71088070552b6c9
huyphamfc <huyphamfc@gmail.com>
2022-09-10 09:28:58 +0700

    TikTok - Header - Form

--- section--02--tiktok/src/components/Layout/components/Header/index.js
+++ section--02--tiktok/src/components/Layout/components/Header/index.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import classes from './Header.module.scss';
 import logoImg from '../../../../assets/images/logo.svg';
 
@@ -9,6 +12,28 @@ function Header() {
                 <div>
                     <img src={logoImg} alt='TikTok logo' />
                 </div>
+                <form className={classes.header__form}>
+                    <input
+                        className={classes.header__input}
+                        placeholder='Search accounts and videos'
+                    />
+                    <button
+                        className={classes.header__clear}
+                        type='button'
+                    >
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <button
+                        className={classes.header__load}
+                        type='button'
+                    >
+                        <FontAwesomeIcon icon={faSpinner} />
+                    </button>
+                    <button className={classes.header__search}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </form>
+                <div></div>
             </nav>
         </header>
     );
