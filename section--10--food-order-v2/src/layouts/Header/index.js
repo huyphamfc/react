2158a47d65182d7721a93a55e7675b9301d1ac6a
huyphamfc <huyphamfc@gmail.com>
2022-10-21 15:07:42 +0700

    Food Order v2 - update Header layout

--- section--10--food-order-v2/src/layouts/Header/index.js
+++ section--10--food-order-v2/src/layouts/Header/index.js
@@ -1,4 +1,3 @@
-import { Section } from '../../UI';
 import { Button } from '../../components';
 
 import style from './Header.module.scss';
@@ -6,12 +5,12 @@ import style from './Header.module.scss';
 function Header() {
   return (
     <header className={style.header}>
-      <Section className={style.header__content}>
+      <div className={style.header__content}>
         <h1 className={style.header__brand}>Food Order</h1>
         <Button type="solid">
           Your Cart <span className={style['header__btn-badge']}>2</span>
         </Button>
-      </Section>
+      </div>
     </header>
   );
 }
